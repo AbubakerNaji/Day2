49eb498aee484ad87cd8174a564c3ec872597458
Abubaker Naji <abubaker.fg@gmail.com>
2025-01-21 11:48:41 +0800

    added a new project

---
+++ secondApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Stack<int> stack = new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
+
+
+
+Console.WriteLine("\nStack:");  
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
